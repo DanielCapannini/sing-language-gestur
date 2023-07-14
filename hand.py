8546328869cb348844a10d093a9b619f5fe4f7df
DanielCapannini <daniel.capannini@studio.unibo.it>
2023-07-14 11:40:52 +0200

    create function prediction

--- hand.py
+++ hand.py
@@ -1,3 +1,17 @@
 import mediapipe as mp
-import cv2
+import cv2 as cv
 import numpy as np
+from tensorflow.keras.models import Sequential, load_model
+
+model_save="./model.hdf5"
+classes = ['A', 'B', 'C', 'D', 'del', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+           'M', 'N', 'nothing', 'O', 'P', 'Q', 'R', 'S', 'space', 'T', 'U', 'V',
+           'W', 'X', 'Y', 'Z']
+
+model=load_model(model_save)
+
+def predition(img):
+    img=cv.resize(img, (48,48))
+    img=img.astype('float32')/255
+    pred=model.predict(img)
+    return classes[np.argmax(pred)], pred
